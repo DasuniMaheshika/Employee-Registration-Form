b33351cf7acddfb03f09af08fbe8fd1643481858
DasuniMaheshika <dasu.maheshika@gmail.com>
2023-05-08 23:05:55 +0530

    Added App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import AddForm from './components/AddForm';
+import EditForm from './components/EditForm';
+import Table from './components/Table';
 import './App.css';
 
+type Item = {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  departmentName: string;
+};
+
 function App() {
+  const [items, setItems] = useState<Item[]>([]);
+  const [editingItem, setEditingItem] = useState<Item | null>(null);
+
+  useEffect(() => {
+    const storedItems = JSON.parse(localStorage.getItem('items') || '[]');
+    setItems(storedItems);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
+
+  function handleAdd(item: Item) {
+    setItems([...items, item]);
+  }
+
+  function handleSave(item: Item) {
+    const index = items.findIndex((i) => i.id === item.id);
+    const newItems = [...items];
+    newItems[index] = item;
+    setItems(newItems);
+    setEditingItem(null);
+  }
+
+  function handleDelete(id: string) {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="">
+      {editingItem ? (
+        <EditForm item={editingItem} onSave={handleSave} onCancel={() => setEditingItem(null)} />
+      ) : (
+        <>
+          <AddForm onAdd={handleAdd} />
+          <Table items={items} onEdit={(item) => setEditingItem(item)} onDelete={handleDelete} />
+        </>
+      )}
+    </Container>
   );
 }
 
